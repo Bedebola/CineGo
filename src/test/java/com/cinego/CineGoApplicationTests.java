8ee8706cbc47422d5a1e1186933836ac81149c16
Berenice Rodrigues <120614169+Bedebola@users.noreply.github.com>
2025-08-31 17:59:35 -0300

    first commit

---
+++ src/test/java/com/cinego/CineGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
